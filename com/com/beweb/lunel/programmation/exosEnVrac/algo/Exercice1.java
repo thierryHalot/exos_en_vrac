14e48298bae5998472eba526cbd5658f1e8a6dcd
thierry <thierry@beweb-16.176R>
2018-03-11 18:10:31 +0100

    ajout de l'exercice 1

---
+++ com/com/beweb/lunel/programmation/exosEnVrac/algo/Exercice1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beweb.lunel.programmation.exosEnVrac.algo;
+
+/**
+ *
+ * @author thierry
+ */
+public class Exercice1 {
+    
+    public static void launch(){
+        // j'ecris mon code ici !
+       boolean empty = false ;
+       boolean fully = true ;
+       
+        System.out.println("exercice 1 reussi");
+       
+    }
+    
+    
+}
